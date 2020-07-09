6b8442fb0dd69ed0fb816eeccf21287a747a6fd8
Anthony Guerrero <adguerrero1935@gmail.com>
2020-07-09 19:50:11 -0400

    Worked on contact page. changed the format to the links

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -7,7 +7,7 @@ class EmailMe extends React.Component {
 
     // LinkedIn() {
     //     alert("LinkedIn clicked on");
-   
+
     navigateToGoogle(website) {
         window.open("https://www.google.com/")
     };
@@ -21,19 +21,30 @@ class EmailMe extends React.Component {
     };
 
 
+
+
     render() {
         return (
             <div>
                 <Container className="containerHolder">
                     <h2 className="Title text-center"> You Can Reach me at</h2>
                     <hr />
-                    <h3 className="email text-center" onClick={() => this.navigateToGoogle("https://www.google.com/")} >Email: adguerrero1935@gmail.com</h3>
-                    <h3 className="LinkedIn text-center" onClick={() => this.navigateToLinkedIn("https://www.linkedin.com/in/anthony-guerrero-63a3a8199/")} >LinkedIn: https://www.linkedin.com/in/anthony-guerrero-63a3a8199/</h3>
-                    <h3 className="Github text-center" onClick={() => this.navigateToGitHub("https://github.com/Anthony-G89")} >GitHub: https://github.com/Anthony-G89 </h3>
+                    <div className="email text-center" onClick={() => this.navigateToGoogle("https://www.google.com/")}>
+                        <h3 style={{ color: "black" }} >Email:</h3><h2 className="myEmail">adguerrero1935@gmail.com</h2>
+
+                    </div>
+                    <div className="LinkedIn text-center" onClick={() => this.navigateToLinkedIn("https://www.linkedin.com/in/anthony-guerrero-63a3a8199/")} >
+                        <h3 style={{ color: "black" }}>LinkedIn:</h3><h2 className="myLinkedIn"> https://www.linkedin.com/in/anthony-guerrero-63a3a8199/</h2>
+                    </div>
+                    
+                    <div className="Github text-center" onClick={() => this.navigateToGitHub("https://github.com/Anthony-G89")} >
+                        <h3 style={{ color: "black" }}>Github:</h3><h2 className="myGitHub">https://github.com/Anthony-G89 </h2>
+                    </div>
                 </Container>
             </div>
 
         )
+
     }
 };
 
