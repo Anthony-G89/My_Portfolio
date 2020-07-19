279ee59987b9ff79ce3879adf51d9398aa728330
Anthony Guerrero <adguerrero1935@gmail.com>
2020-07-19 13:34:36 -0400

    Chnaged color in Contact

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -27,18 +27,18 @@ class EmailMe extends React.Component {
         return (
             <div>
                 <Container className="containerHolder">
-                    <h1 className="Title text-center" style={{color:"red"}}> Contact Me</h1>
+                    <h1 className="Title text-center" style={{color:"green"}}> Contact Me</h1>
                     <hr />
                     <div className="email text-center" onClick={() => this.navigateToGoogle("https://www.google.com/")}>
-                        <h3 style={{ color: "red" }} >Email:</h3><h2 className="myEmail">adguerrero1935@gmail.com</h2>
+                        <h3 style={{ color: "green" }} >Email:</h3><h2 className="myEmail">adguerrero1935@gmail.com</h2>
 
                     </div>
                     <div className="LinkedIn text-center" onClick={() => this.navigateToLinkedIn("https://www.linkedin.com/in/anthony-guerrero-63a3a8199/")} >
-                        <h3 style={{ color: "red" }}>LinkedIn:</h3><h2 className="myLinkedIn"> https://www.linkedin.com/in/anthony-guerrero-63a3a8199/</h2>
+                        <h3 style={{ color: "green" }}>LinkedIn:</h3><h2 className="myLinkedIn"> https://www.linkedin.com/in/anthony-guerrero-63a3a8199/</h2>
                     </div>
 
                     <div className="Github text-center" onClick={() => this.navigateToGitHub("https://github.com/Anthony-G89")} >
-                        <h3 style={{ color: "red" }}>Github:</h3><h2 className="myGitHub">https://github.com/Anthony-G89 </h2>
+                        <h3 style={{ color: "green" }}>Github:</h3><h2 className="myGitHub">https://github.com/Anthony-G89 </h2>
                     </div>
                 </Container>
             </div>
