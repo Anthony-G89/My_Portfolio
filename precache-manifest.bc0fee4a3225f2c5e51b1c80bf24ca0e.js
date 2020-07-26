6ee4567fce04b499525b7db51e3c832f7ff4535e
Anthony Guerrero <adguerrero1935@gmail.com>
2020-07-26 12:15:08 -0400

    Updates

---
+++ precache-manifest.bc0fee4a3225f2c5e51b1c80bf24ca0e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f4665b06bf04b2fe124f177b6a6980",
+    "url": "/My_Portfolio/index.html"
+  },
+  {
+    "revision": "aedac9f8c9132433710b",
+    "url": "/My_Portfolio/static/css/main.4d6177ba.chunk.css"
+  },
+  {
+    "revision": "8d7cf95caa12def91ace",
+    "url": "/My_Portfolio/static/js/2.019a3c91.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/My_Portfolio/static/js/2.019a3c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedac9f8c9132433710b",
+    "url": "/My_Portfolio/static/js/main.01195194.chunk.js"
+  },
+  {
+    "revision": "b8e14fb01c186625e5ea",
+    "url": "/My_Portfolio/static/js/runtime-main.5ae618d6.js"
+  },
+  {
+    "revision": "05aa44a1762efa653a8615756e71e9c9",
+    "url": "/My_Portfolio/static/media/Burgers.05aa44a1.png"
+  },
+  {
+    "revision": "2d1643553e4079a435c691bfbf06d030",
+    "url": "/My_Portfolio/static/media/Code Quiz.2d164355.png"
+  },
+  {
+    "revision": "3385ec056bc6e7909ba10f7b39839886",
+    "url": "/My_Portfolio/static/media/Local Wire.3385ec05.png"
+  },
+  {
+    "revision": "d8bf060a383af5d3bb87934980c42464",
+    "url": "/My_Portfolio/static/media/MeetHub.d8bf060a.png"
+  },
+  {
+    "revision": "f7749b299d19b6c59c1e94f2031056e0",
+    "url": "/My_Portfolio/static/media/STACK_CHAT.f7749b29.png"
+  },
+  {
+    "revision": "444cea35534af4a9609c903b97c2712f",
+    "url": "/My_Portfolio/static/media/double-bubble-dark.444cea35.png"
+  },
+  {
+    "revision": "1aabd70d1a3c2ff1e6177c76cee9fb5b",
+    "url": "/My_Portfolio/static/media/employee_directory.1aabd70d.png"
+  },
+  {
+    "revision": "7448261f704b4bcc92d8055e46140dfe",
+    "url": "/My_Portfolio/static/media/home_fitness_tracker.7448261f.png"
+  },
+  {
+    "revision": "83ae6d240d22b22db984f8d3ce596b3c",
+    "url": "/My_Portfolio/static/media/home_week11_noteTaker.83ae6d24.png"
+  },
+  {
+    "revision": "d4538a78c84f9c3b2fed792acbd51e21",
+    "url": "/My_Portfolio/static/media/password_generator.d4538a78.png"
+  },
+  {
+    "revision": "504f3e8e82fefebbf4b73969149e5be8",
+    "url": "/My_Portfolio/static/media/showingFiveDayForeCast(2).504f3e8e.png"
+  }
+]);
